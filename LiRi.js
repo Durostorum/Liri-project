78c670ff532cc6e559887e4f7384d7aaa9826c2f
Georgi <georgi.nikolow@gmail.com>
2019-08-21 14:31:28 -0400

    movie-this function

--- LiRi.js
+++ LiRi.js
@@ -9,7 +9,32 @@ var spotify = new Spotify(keys.spotify)
 
 
 
-
+// MOVIE_THIS FUNCTION
+function omdbfunction () {
+    var movieName = [];
+    for ( var i = 3; i < process.argv.length; i++){
+        movieName.push(process.argv[i])
+    } if (movieName == ""){
+        movieName = "Mr Nobody"
+        console.log("_____________________\n\nIt's on NETFLIX TOO\n_____________________")
+    }
+  //MOVIE_THIS API REQUEST
+  axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(function(response) {
+      
+      var movieInfo = response.data;
+      var printMovie = 
+      "--------------------------------------------------------------------" +
+          "\n Name: " + movieInfo.Title + 
+          "\n\n imdbRating: " + movieInfo.imdbRating +
+          "\n Rotten Tomatoes: " + movieInfo.Ratings[1].Source + ' ' + movieInfo.Ratings[1].Value +
+          "\n Country: " + movieInfo.Country +
+          "\n Language: " + movieInfo.Language +
+          "\n Plot: " + movieInfo.Plot +
+          "\n Actors: " + movieInfo.Actors +
+          "\n\n--------------------------------------------------------------------" ;
+      console.log(printMovie)
+  })
+  }
 
 
 
